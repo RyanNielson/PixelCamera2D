3ca2f54fb3808fb92329d5354de2be79a1e97ff3
Ryan Nielson <ryan.nielson@gmail.com>
2015-09-21 15:47:29 -0300

    Added prefabs and demo tweaks.

---
+++ Assets/PixelCamera2DDemo/Scripts/Rotator.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    [SerializeField]
+    private float degreesPerSecond = 20f;
+
+	private void Update()
+    {
+        transform.Rotate(Vector3.forward * degreesPerSecond * Time.deltaTime);
+    }
+}
